8b624bcf281e481d55fd83ad5711f5d19983929e
shotwn <anilhaksever@gmail.com>
2023-11-03 04:04:15 +0300

    chore: update tempermonkey header

--- src/tempermonkey-development-header.js
+++ src/tempermonkey-development-header.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Özge's Youtube Translator - Development
 // @namespace    https://anilhaksever.com
-// @version      0.0.1
+// @version      0.1.1
 // @description  Translates description and title of videos in Youtube Studio using Google Translate API
 // @author       shotwn
 // @match        https://studio.youtube.com/**
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @icon         https://github.com/shotwn/Ozges-Youtube-Studio-Translator/raw/main/logo.png
 // @grant        GM.xmlHttpRequest
 // @grant        GM.setValue
 // @grant        GM.getValue
--- src/tempermonkey-header.js
+++ src/tempermonkey-header.js
@@ -1,14 +1,16 @@
 // ==UserScript==
 // @name         Özge's Youtube Translator
 // @namespace    https://anilhaksever.com
-// @version      0.0.1
+// @version      0.1.1
 // @description  Translates description and title of videos in Youtube Studio using Google Translate API
 // @author       shotwn
 // @match        https://studio.youtube.com/**
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @icon         https://github.com/shotwn/Ozges-Youtube-Studio-Translator/raw/main/logo.png
 // @grant        GM.xmlHttpRequest
 // @grant        GM.setValue
 // @grant        GM.getValue
 // @connect      translation.googleapis.com
 // @supportURL   https://github.com/shotwn/Ozges-Youtube-Studio-Translator/issues
+// @updateURL    https://github.com/shotwn/Ozges-Youtube-Studio-Translator/releases/latest/download/tempermonkey-userscript.js
+// @downloadURL  https://github.com/shotwn/Ozges-Youtube-Studio-Translator/releases/latest/download/tempermonkey-userscript.js
 // ==/UserScript==
